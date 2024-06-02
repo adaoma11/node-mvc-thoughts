3e78f9351951448c2fbdc39d7fbc15ad77864576
Adão Araujo <adaoma11@hotmail.com>
2024-06-02 00:09:10 -0300

    feat: add edit thought function

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -83,4 +83,54 @@ module.exports = class ToughtController {
             console.error("Ocorreu um erro ao remover o pensamento: " + err);
         }
     }
+
+    static async updateTought(req, res) {
+        const toughtId = req.params.id;
+        const userId = req.session.userid;
+
+        try {
+            const tought = await Tought.findOne({
+                where: { id: toughtId, UserId: userId },
+                raw: true,
+            });
+
+            res.render("toughts/edit", { tought });
+        } catch (err) {
+            console.error("Ocorreu um erro ao buscar o pensamento: " + err);
+        }
+    }
+
+    static async updateToughtSave(req, res) {
+        if (!req.session.userid) {
+            res.redirect("/login");
+            return;
+        }
+
+        const userId = req.session.userid;
+        const toughtId = req.body.id;
+
+        const tought = {
+            title: req.body.title,
+        };
+
+        if (!req.body.title || req.body.title.length < 3) {
+            req.flash("message", "Caracteres insuficientes");
+            res.render("toughts/edit", { tought });
+            return;
+        }
+
+        try {
+            await Tought.update(tought, {
+                where: { id: toughtId, UserId: userId },
+            });
+
+            req.flash("message", "Pensamento atualizado com sucesso");
+
+            req.session.save(() => {
+                res.redirect("/toughts/dashboard");
+            });
+        } catch (err) {
+            console.error("Ocorreu um erro ao atualizar o pensamento: " + err);
+        }
+    }
 };
